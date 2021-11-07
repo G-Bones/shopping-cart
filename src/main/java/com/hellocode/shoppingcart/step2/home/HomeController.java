1e454b5f4f09fbb2ec5790a830ad9d2d5c5b3ee7
Marta Rey <martarey@Martas-MacBook-Pro.local>
2021-11-07 17:11:59 +0000

    First commit

---
+++ src/main/java/com/hellocode/shoppingcart/step2/home/HomeController.java
@@ -0,0 +1,31 @@
+package com.hellocode.shoppingcart.step2.home;
+
+import com.hellocode.shoppingcart.step2.domain.Product;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class HomeController {
+
+    @FXML
+    private GridPane productGridPane;
+
+    @FXML
+    public void initialize(){
+
+        productGridPane.getChildren().clear();
+        Label label1 = new Label(Product.APPLE.name());
+        label1.setPadding(new Insets(5,5,5,5));
+        productGridPane.add(label1,0,0);
+        Label label2 = new Label(Product.MILK.name());
+        label2.setPadding(new Insets(5,5,5,5));
+        productGridPane.add(label2,1,0);
+        Label label3 = new Label(Product.LETTUCE.name());
+        label3.setPadding(new Insets(5,5,5,5));
+        productGridPane.add(label3,2,0);
+        Label label4 = new Label(Product.JUICE.name());
+        label4.setPadding(new Insets(5,5,5,5));
+        productGridPane.add(label4,0,1);
+    }
+}
